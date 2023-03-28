bdbb859e76e1468dc2ffba140761f6796a7a2a92
Billy Gibendi <ralphgibendi01@gmail.com>
2023-03-28 21:52:13 +0300

    choice function

--- Python-Essentials-2.py
+++ Python-Essentials-2.py
@@ -73,8 +73,29 @@ randrange(end)
 randrange(beg, end)
 randrange(beg, end, step)
 
+from random import randrange, randint
+
+print(randrange(1), end=' ')
+print(randrange(0, 1), end=' ')
+print(randrange(0, 1, 1), end=' ')
+print(randint(0, 1))
+
+# they may produce repeating values even if the number of subsequent invocations is not greater than the width of the specified range.
+
 # The randint function returns a random integer from the range specified by the user.
-randint()
+from random import randint
+
+for i in range(10):
+    print(randint(1, 10), end=',')
+
+#  choice and sample functions
+from random import choice, sample
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(choice(my_list))
+print(sample(my_list, 5))
+print(sample(my_list, 10))
 
 
 
--- tester.py
+++ tester.py
@@ -1,5 +1,5 @@
 from random import randrange, randint
 
 randrange(end)
-randrange(beg, end)
+randrange(beg, end) # beg implies beginning of range, while end implies the end of the range
 randrange(beg, end, step)
