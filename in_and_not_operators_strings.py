39d18fe2797b582ac848fa0fa0f6f8c8638f0eb9
Billy Gibendi <ralphgibendi01@gmail.com>
2023-04-21 23:10:44 +0300

    string operations

---
+++ in_and_not_operators_strings.py
@@ -0,0 +1,31 @@
+# the "in" and "not in" operators with strings
+
+# "in" operator
+# returns True if the string is found in the string
+# returns False if the string is not found in the string
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+print("f" in alphabet)
+print("F" in alphabet)
+print("1" in alphabet)
+print("ghi" in alphabet)
+print("Xyz" in alphabet)
+
+
+# break line and go to next line
+print()
+
+# "not in" operator
+# returns True if the string is not found in the string
+# returns False if the string is found in the string
+
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+print("f" not in alphabet)
+print("F" not in alphabet)
+print("1" not in alphabet)
+print("ghi" not in alphabet)
+print("Xyz" not in alphabet)
+
