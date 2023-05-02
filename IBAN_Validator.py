3e4b197d5effb3dce7fd2234b6bc4c6ed206b660
Billy Gibendi <ralphgibendi01@gmail.com>
2023-05-02 16:25:11 +0300

    📃 docs: update 2 files and create 2 files

--- IBAN_Validator.py
+++ IBAN_Validator.py
@@ -1,6 +1,18 @@
 # IBAN Validator
 # implements (in a slightly simplified form) an algorithm used by European banks to specify account numbers
 
+# An IBAN-compliant account number consists of:
+
+# A two-letter country code taken from the ISO 3166-1 standard (e.g., FR for France, GB for Great Britain, DE for Germany, and so on)
+# two check digits used to perform the validity checks – fast and simple, but not fully reliable, tests, showing whether a number is invalid (distorted by a typo) or seems to be good;
+# the actual account number (up to 30 alphanumeric characters – the length of that part depends on the country)
+
+# The standard says that validation requires the following steps:
+
+# (step 1) Check that the total IBAN length is correct as per the country (this program won't do that, but you can modify the code to meet this requirement if you wish; note: you have to teach the code all the lengths used in Europe)
+# (step 2) Move the four initial characters to the end of the string (i.e., the country code and the check digits)
+# (step 3) Replace each letter in the string with two digits, thereby expanding the string, where A = 10, B = 11 ... Z = 35;
+# (step 4) Interpret the string as a decimal integer and compute the remainder of that number by modulo-dividing it by 97; If the remainder is 1, the check digit test is passed and the IBAN might be valid.
 
 # Importing the necessary modules
 import re
@@ -43,4 +55,11 @@ print(IBAN_Validator('GB82WEST1234569876543A'))
 
 print(IBAN_Validator('GB82 WEST 1234 5698 7654 32'))
 
-print(IBAN_Validator('GB82WEST12345698765432 '))
+print(IBAN_Validator('DE02100100100152517108')) # German
+
+print(IBAN_Validator('FR76 30003 03620 00020216907 50')) # French
+
+print(IBAN_Validator('GB72 HBZU 7006 7212 1253 00')) # British
+
+
+ 
---
+++ Palindromes.py
@@ -0,0 +1,24 @@
+# It's a word which look the same when read forward and backward. 
+# For example, "kayak" is a palindrome, while "loyal" is not.
+
+
+# This program:
+# Asks the user for some text;
+# Checks whether the entered text is a palindrome, and prints the result.
+
+# Note
+# assume that an empty string isn't a palindrome;
+# treat upper- and lower-case letters as equal;
+# spaces are not taken into account during the check – treat them as non-existent;
+# there are more than a few correct solutions – try to find more than one.
+
+text = input("What words do you have in mind: ")
+
+# Remove all spaces...
+text = text.replace(' ','')
+
+# ... and check if the word is equal to reversed itself
+if len(text) > 1 and text.upper() == text[::-1].upper():
+	print("It's a palindrome")
+else:
+	print("It's not a palindrome")
