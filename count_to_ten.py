be960c3029182a0c7abb664746fb0cb5328cbf71
Billy Gibendi <ralphgibendi01@gmail.com>
2023-10-21 14:04:58 +0400

    🎉 init: counting scripts

---
+++ count_to_ten.py
@@ -0,0 +1,7 @@
+def count_to_ten():
+  # Loop through the numbers from first to last 
+    x = 0
+    while x <= 10:
+        x += 1
+        print(x)
+count_to_ten()
---
+++ expressions.py
@@ -0,0 +1,8 @@
+# program that prompts the user for an arithmetic expression calculates and outputs the result as a floating-point value formatted to one decimal place.
+
+expression = input("Expression:")
+
+
+
+# Assume that the user’s input will be formatted as x y z, with one space between x and y and one space between y and z, wherein:
+
